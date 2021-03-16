c61a90820db304b0a5ee838d9613ebd727bce5ba
MadhavanKR <madhavan.kalkunte@gmail.com>
2021-03-16 21:58:38 +0530

    added react router

---
+++ src/components/MissingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MissingPage extends React.Component {
+
+    render() {
+        console.log("in missing component")
+        return (
+            <h1> MissingPage </h1>
+        )
+    }
+
+}
+
+export default MissingPage;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
-
-import * as reactfa from 'react-icons/fa'
+import { Icon, Menu, Segment, Sidebar } from 'semantic-ui-react'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import Profile from './Profile'
+import Home from './Home'
+import MissingPage from './MissingPage'
 
 class NavigationBar extends React.Component {
 
@@ -25,37 +27,43 @@ class NavigationBar extends React.Component {
                 <div className="ui menu top attached inverted">
                     <a className="item" onClick={this.changeVisibility}>Menu</a>
                 </div>
-                <Sidebar.Pushable style={{height: '100vh'}}>
-                    <Sidebar
-                        as={Menu}
-                        animation='overlay'
-                        icon='labeled'
-                        inverted
-                        onHide={() => this.setState({ visible: false })}
-                        vertical
-                        visible={this.state.visible}
-                        width='thin'
-                        
-                    >
-                        <Menu.Item as='a'>
-                            <Icon name='home' />
+                <BrowserRouter>
+                    <Sidebar.Pushable style={{ height: '100vh' }}>
+                        <Sidebar
+                            as={Menu}
+                            animation='overlay'
+                            icon='labeled'
+                            inverted
+                            onHide={() => this.setState({ visible: false })}
+                            vertical
+                            visible={this.state.visible}
+                            width='thin'
+
+                        >
+                            <Menu.Item as={Link} to="/" onClick={this.closeMenu}>
+                                <Icon name='home' />
           Home
-        </Menu.Item>
-                        <Menu.Item as='a'>
-                            <Icon name='gamepad' />
+        </Menu.Item >
+                            <Menu.Item as={Link} to="/profile" onClick={this.closeMenu}>
+                                <Icon name='gamepad' />
           Games
-        </Menu.Item>
-                        <Menu.Item as='a'>
-                            <Icon name='camera' />
+        </Menu.Item >
+                            <Menu.Item as={Link} to="/camera" onClick={this.closeMenu}>
+                                <Icon name='camera' />
           Channels
-        </Menu.Item>
-                    </Sidebar>
-                    <Sidebar.Pusher dimmed={this.state.visible}>
-                        <Segment basic >
-                            <Header as='h3'>Application Content</Header>
-                        </Segment>
-                    </Sidebar.Pusher>
-                </Sidebar.Pushable>
+        </Menu.Item >
+                        </Sidebar>
+                        <Sidebar.Pusher dimmed={this.state.visible}>
+                            <Segment basic >
+                                <Switch>
+                                    <Route component={Home} exact path="/"></Route>
+                                    <Route component={Profile} exact path="/profile"></Route>
+                                    <Route component={MissingPage} />
+                                </Switch>
+                            </Segment>
+                        </Sidebar.Pusher>
+                    </Sidebar.Pushable>
+                </BrowserRouter>
             </>
         );
 
